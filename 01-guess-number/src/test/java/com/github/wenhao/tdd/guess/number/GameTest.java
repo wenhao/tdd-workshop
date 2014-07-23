effdcb068382e04dd000c4abe4c197a3644b43ae
Hao Wen <wenhao@126.com>
2014-07-23 22:26:32 +0800

    [Wen Hao] - add more tests to cover compare answer.

--- 01-guess-number/src/test/java/com/github/wenhao/tdd/guess/number/GameTest.java
+++ 01-guess-number/src/test/java/com/github/wenhao/tdd/guess/number/GameTest.java
@@ -1,6 +1,7 @@
 package com.github.wenhao.tdd.guess.number;
 
 import com.github.wenhao.tdd.guess.number.domain.Answer;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -8,12 +9,19 @@ import static org.junit.Assert.assertThat;
 
 public class GameTest
 {
+
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        game = new Game(Answer.createAnswer("1 2 3 4"));
+    }
+
     @Test
     public void should_return_0A0B_if_no_number_is_correct() throws Exception
     {
         // given
-        Answer actualAnswer = Answer.createAnswer("1 2 3 4");
-        Game game = new Game(actualAnswer);
         Answer inputAnswer = Answer.createAnswer("5 6 7 8");
 
         // when
@@ -23,4 +31,55 @@ public class GameTest
         assertThat(result, is("0A0B"));
     }
 
+    @Test
+    public void should_return_4A0B_if_all_number_are_correct() throws Exception
+    {
+        // given
+        Answer inputAnswer = Answer.createAnswer("1 2 3 4");
+
+        // when
+        String result = game.guess(inputAnswer);
+
+        // then
+        assertThat(result, is("4A0B"));
+    }
+
+    @Test
+    public void should_be_able_to_return_0A2B() throws Exception
+    {
+        // given
+        Answer inputAnswer = Answer.createAnswer("2 4 7 8");
+
+        // when
+        String result = game.guess(inputAnswer);
+
+        // then
+        assertThat(result, is("0A2B"));
+    }
+
+    @Test
+    public void should_be_able_to_return_1A2B() throws Exception
+    {
+        // given
+        Answer inputAnswer = Answer.createAnswer("0 3 2 4");
+
+        // when
+        String result = game.guess(inputAnswer);
+
+        // then
+        assertThat(result, is("1A2B"));
+    }
+
+    @Test
+    public void should_be_able_to_return_0A4B() throws Exception
+    {
+        // given
+        Answer inputAnswer = Answer.createAnswer("4 3 2 1");
+
+        // when
+        String result = game.guess(inputAnswer);
+
+        // then
+        assertThat(result, is("0A4B"));
+    }
 }
