16637edbf41c4bc9ffa8b202c44182a2d27217f5
Wen Hao <wenhao@126.com>
2017-04-23 17:45:53 +0800

    [Wen Hao] - refactor.

--- 01-guess-number/src/main/java/com/github/wenhao/tdd/guess/number/domain/Answer.java
+++ 01-guess-number/src/main/java/com/github/wenhao/tdd/guess/number/domain/Answer.java
@@ -1,11 +1,8 @@
 package com.github.wenhao.tdd.guess.number.domain;
 
-import com.google.common.base.Predicate;
-
 import java.util.List;
 
 import static com.github.wenhao.tdd.guess.number.domain.AnswerConstant.ANSWER_SEPARATOR;
-import static com.google.common.collect.FluentIterable.from;
 import static com.google.common.collect.Lists.newArrayList;
 
 public class Answer
@@ -29,34 +26,26 @@ public class Answer
         final List<String> actualNumbers = newArrayList(value.split(ANSWER_SEPARATOR));
         final List<String> inputNumbers = newArrayList(answer.value.split(ANSWER_SEPARATOR));
         return new StringBuilder()
-                .append(correctNumber(actualNumbers, inputNumbers))
+                .append(correctNumberAndPosition(actualNumbers, inputNumbers))
                 .append(CORRECT_NUMBER)
-                .append(mixNumber(actualNumbers, inputNumbers))
+                .append(correctNumberButWrongPosition(actualNumbers, inputNumbers))
                 .append(MIX_NUMBER).toString();
     }
 
-    private Integer mixNumber(final List<String> actualNumbers, final List<String> inputNumbers)
+    private Integer correctNumberButWrongPosition(final List<String> actualNumbers, final List<String> inputNumbers)
     {
-        return from(inputNumbers).filter(new Predicate<String>()
-        {
-            @Override
-            public boolean apply(String number)
-            {
-                return actualNumbers.contains(number) && actualNumbers.indexOf(number) != inputNumbers.indexOf(number);
-            }
-        }).toList().size();
+        Long count = inputNumbers.stream()
+                .filter(number -> actualNumbers.contains(number) && actualNumbers.indexOf(number) != inputNumbers.indexOf(number))
+                .count();
+        return count.intValue();
     }
 
-    private Integer correctNumber(final List<String> actualNumbers, final List<String> inputNumbers)
+    private Integer correctNumberAndPosition(final List<String> actualNumbers, final List<String> inputNumbers)
     {
-        return from(inputNumbers).filter(new Predicate<String>()
-        {
-            @Override
-            public boolean apply(String number)
-            {
-                return actualNumbers.contains(number) && actualNumbers.indexOf(number) == inputNumbers.indexOf(number);
-            }
-        }).toList().size();
+        Long count = inputNumbers.stream()
+                .filter(number -> actualNumbers.contains(number) && actualNumbers.indexOf(number) == inputNumbers.indexOf(number))
+                .count();
+        return count.intValue();
     }
 
     public String getValue()
