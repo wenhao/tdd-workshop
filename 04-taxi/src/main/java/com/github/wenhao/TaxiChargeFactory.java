368bd2e4f37d4ee9007a04158cadcbd2271a93f4
Wen Hao <wenhao@126.com>
2017-05-13 11:43:34 +0800

    [Wen Hao] - task 19.
    (cherry picked from commit 7eb07c6)

--- 04-taxi/src/main/java/com/github/wenhao/TaxiChargeFactory.java
+++ 04-taxi/src/main/java/com/github/wenhao/TaxiChargeFactory.java
@@ -6,13 +6,13 @@ import static java.math.BigDecimal.ROUND_UP;
 
 public class TaxiChargeFactory
 {
+    private static final BigDecimal DAY_BASE_FEE = BigDecimal.valueOf(11).setScale(2, ROUND_UP);
+    private static final BigDecimal PRICE_PER_MILE = BigDecimal.valueOf(1.6D);
+    private static final BigDecimal NIGHT_BASE_FEE = BigDecimal.valueOf(13).setScale(2, ROUND_UP);
+    private static final BigDecimal NIGHT_PRICE_PER_MILE = BigDecimal.valueOf(2.4D);
 
     public TaxiCharge getTaxiCharge()
     {
-        final BigDecimal DAY_BASE_FEE = BigDecimal.valueOf(11).setScale(2, ROUND_UP);
-        final BigDecimal PRICE_PER_MILE = BigDecimal.valueOf(1.6D);
-        final BigDecimal NIGHT_BASE_FEE = BigDecimal.valueOf(13).setScale(2, ROUND_UP);
-        final BigDecimal NIGHT_PRICE_PER_MILE = BigDecimal.valueOf(2.4D);
         final TaxiCharge dayCharge = new CompositeTaxiCharge().withBaseFee(DAY_BASE_FEE).withAdditionalFee(PRICE_PER_MILE);
         final TaxiCharge nightCharge = new CompositeTaxiCharge().withBaseFee(NIGHT_BASE_FEE).withAdditionalFee(NIGHT_PRICE_PER_MILE);
         return new TimeTaxiCharge(dayCharge, nightCharge);
