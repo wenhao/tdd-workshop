c8ca491a965b74d2006b2082d3f7167783b2d267
Wen Hao <wenhao@126.com>
2017-05-14 09:18:43 +0800

    [Wen Hao] - add test.
    (cherry picked from commit 742fa91)

---
+++ 04-taxi/src/test/java/com/github/wenhao/factory/MultipleTaxiChargeFactoryTest.java
@@ -0,0 +1,59 @@
+package com.github.wenhao.factory;
+
+import com.github.wenhao.charge.TaxiCharge;
+import com.github.wenhao.config.TaxiConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MultipleTaxiChargeFactoryTest
+{
+
+    private MultipleTaxiChargeFactory taxiChargeFactory;
+    @Mock
+    private TaxiConfig taxiConfig;
+
+    @Before
+    public void setUp()
+    {
+        taxiChargeFactory = new MultipleTaxiChargeFactory(taxiConfig);
+    }
+
+    @Test
+    public void should_get_day_charge()
+    {
+        // given
+        when(taxiConfig.get("normal", "day", "baseFee")).thenReturn("11");
+        when(taxiConfig.get("normal", "day", "range")).thenReturn("3");
+        when(taxiConfig.get("normal", "day", "pricePerMile.3")).thenReturn("1.6");
+
+        // when
+        TaxiCharge dayCharge = taxiChargeFactory.getDayCharge("normal");
+
+        // then
+        assertThat(dayCharge, notNullValue());
+    }
+
+    @Test
+    public void should_get_night_charge()
+    {
+        // given
+        when(taxiConfig.get("normal", "night", "baseFee")).thenReturn("11");
+        when(taxiConfig.get("normal", "night", "range")).thenReturn("3");
+        when(taxiConfig.get("normal", "night", "pricePerMile.3")).thenReturn("1.6");
+
+        // when
+        TaxiCharge nightCharge = taxiChargeFactory.getNightCharge("normal");
+
+        // then
+        assertThat(nightCharge, notNullValue());
+
+    }
+}
