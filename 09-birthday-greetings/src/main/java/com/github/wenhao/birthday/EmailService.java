b2d04d6e40f51487543c5997e89ff6f76413fb15
Wen Hao <wenhao@126.com>
2019-03-11 13:47:23 +0800

    [TECH][Wen Hao] - add task 4.

--- 09-birthday-greetings/src/main/java/com/github/wenhao/birthday/EmailService.java
+++ 09-birthday-greetings/src/main/java/com/github/wenhao/birthday/EmailService.java
@@ -3,6 +3,7 @@ package com.github.wenhao.birthday;
 public class EmailService {
 
     public void send(final Email email) {
-
+        System.out.println(email.getSubject());
+        System.out.println(email.getContent());
     }
 }
