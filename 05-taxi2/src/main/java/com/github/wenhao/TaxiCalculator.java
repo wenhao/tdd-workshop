54fda92b64b3ae5649d234fd95fe81fa42114566
wenhao <wenhao@126.com>
2023-05-26 09:53:37 +0800

    update

--- 05-taxi2/src/main/java/com/github/wenhao/TaxiCalculator.java
+++ 05-taxi2/src/main/java/com/github/wenhao/TaxiCalculator.java
@@ -4,8 +4,10 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
+import com.google.common.collect.ImmutableList;
+
 public class TaxiCalculator {
-    private final List<FeeCalculator> calculators = Arrays.asList(new StartingFeeCalculator(),
+    private final List<FeeCalculator> calculators = ImmutableList.of(new StartingFeeCalculator(),
             new AdditionalFeeCalculator(),
             new LongDistanceCalculator(),
             new WaitingTimeCalculator());
